8f61191bb008d6e649d0027c5401aa41afc7368f
erfanaskarian <erfan.askarian2003@gmail.com>
2025-10-23 17:02:08 +0200

    ex_0_beginner_runfile.py

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -2,4 +2,4 @@ import this
 
 print("\n"+"My name is Erfan Askarian. I’m learning Python to analyze laboratory data and improve my skills in scientific research.")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"The last time I was without a computer was when my laptop was broken, and I had to wait a few days for it to be repaired.")
