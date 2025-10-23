8f56cb13ffce6c87e5706e71a9d97eaa31eec84a
erfanaskarian <erfan.askarian2003@gmail.com>
2025-10-23 17:00:29 +0200

    ex_0_beginner_runfile.py

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"My name is Erfan Askarian. I’m learning Python to analyze laboratory data and improve my skills in scientific research.")
 
 print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
